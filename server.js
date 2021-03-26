98c06ff911d7f56d0d3aa9ce4e6a10d6bab81733
Mary Papamichalis <mary.papamichalis@gmail.com>
2021-03-25 21:04:05 -0500

    more heroku fixes

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const path= require("path");
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Add routes, both API and view
+app.use(routes);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -15,8 +19,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
 }
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://ibarrasb:pWFl91jlXfpvo2pe@cluster0-shard-00-00.9yudd.mongodb.net:27017,cluster0-shard-00-01.9yudd.mongodb.net:27017,cluster0-shard-00-02.9yudd.mongodb.net:27017/upLinkDB?ssl=true&replicaSet=atlas-ma7mzu-shard-0&authSource=admin&retryWrites=true&w=majority");
