91d6caaab51148e7c0357aded520ade99d6b4be0
alcalaluis <alcalaluis@rocketmail.com>
2021-03-30 01:15:38 -0500

    styling changes to event target page

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -5,7 +5,7 @@ function Jumbotron({ children }) {
   return (
     <div
       style={{ height: 560, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron border border-success"
+      className="jumbotron"
     >
       {children}
     </div>
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -3,7 +3,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 //import CreateEvent from "../components/CreateEvent";
 //import EventList from "../components/Pages/EventList";
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import CardSigup from '../components/CardSignup';
 
 const SignUpPage = () => {
